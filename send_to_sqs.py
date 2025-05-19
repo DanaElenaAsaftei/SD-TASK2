1323e09cfa6d06a97700dc82744bc2e936097bfa
DanaElenaAsaftei <101638765+DanaElenaAsaftei@users.noreply.github.com>
2025-05-19 17:17:11 +0200

    Create send_to_sqs.py

---
+++ send_to_sqs.py
@@ -0,0 +1,18 @@
+import boto3
+
+# URL de tu cola (puedes copiarla desde la consola SQS)
+queue_url = 'https://sqs.us-east-1.amazonaws.com/375257933091/insults-queue'
+
+# Crear cliente SQS
+sqs = boto3.client('sqs', region_name='us-east-1')
+
+
+# Lista de mensajes de prueba
+texts = [
+    "You are a fool",
+    "Idiot! Fix it already!",
+    "Hello world",
+    "Such a nerdy mistake",
+    "What a dumb error",
+    "Don't be a clown"
+]
