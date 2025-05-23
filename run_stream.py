abbd7d9a6cadd26a74cef7f81fbf5b5480f4a2c8
DanaElenaAsaftei <101638765+DanaElenaAsaftei@users.noreply.github.com>
2025-05-23 19:05:49 +0200

    Create run_stream.py

---
+++ run_stream.py
@@ -0,0 +1,7 @@
+from stream import stream
+
+stream(
+    function_name="InsultFilterLambda",  
+    maxfunc=5,                          
+    queue_url="https://sqs.us-east-1.amazonaws.com/375257933091/insults-queue"  
+)
