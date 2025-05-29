e46118164d599744d51acb36a10994579002b5c7
DanaElenaAsaftei <101638765+DanaElenaAsaftei@users.noreply.github.com>
2025-05-29 08:43:03 +0200

    Update send_messages.py

--- send_messages.py
+++ send_messages.py
@@ -1,6 +1,6 @@
 import boto3
-#ej2
-queue_url = "https://sqs.us-east-1.amazonaws.com/058264277608/insult-queue"
+
+queue_url = "https://sqs.us-east-1.amazonaws.com/375257933091/insults-queue"
 
 messages = [
     "You are a dumb clown.",
@@ -11,7 +11,7 @@ messages = [
     "You are a clown and a fool."
 ]
 
-sqs = boto3.client('sqs')
+sqs = boto3.client('sqs', region_name='us-east-1')
 
 for msg in messages:
     sqs.send_message(QueueUrl=queue_url, MessageBody=msg)
